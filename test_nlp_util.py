ad46c76b34605afeaabfc1b3f1cf379ca42151b1
Kent Shih <kent@csie.io>
2021-03-16 18:26:37 +0800

    update

--- test_nlp_util.py
+++ test_nlp_util.py
@@ -15,4 +15,18 @@ def test_get_bio_tagging():
     print(result)
     assert result
 
+def test_getPair():
+    # label, raw = "hanief sembilan mtr -h", "kuripan hanief semb mtr -h, gajah mada, 58112"
+    # print(getPair(label, raw))
 
+    raw = " ".join(['a', 'b', 'c', 'd'])
+    label = " ".join(['ba', '-', 'cc'])
+    assert nlp_util.get_fuzzy_pairs(raw, label) == [1, [('ba', 'b'), ('-', None), ('cc', 'c')]]
+
+    raw = " ".join(['b', '-', 'c', 'd'])
+    label = " ".join(['ba', 'cc'])
+    assert nlp_util.get_fuzzy_pairs(raw, label) == [0, [('ba', 'b'), ('cc', 'c')]]
+
+    raw = ['a', 'b', 'c', 'd']
+    label = ['ba', 'cc']
+    assert nlp_util.get_fuzzy_pairs(raw, label) == [1, [('ba', 'b'), ('cc', 'c')]]
