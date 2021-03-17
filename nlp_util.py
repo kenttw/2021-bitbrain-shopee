257041475584679fc57825ca477f5a1090b24c87
Kent Shih <kent@csie.io>
2021-03-17 11:37:42 +0800

    update

--- nlp_util.py
+++ nlp_util.py
@@ -12,8 +12,8 @@ import re
 poi_label = 'isnar, s.h. & rekan'
 raw_address = 'isn s.h. & rekan, somba opu 76'
 
-
 from fuzzywuzzy import fuzz
+
 
 def prepare_text(text):
     text = re.sub(r'([a-z0-9]+)', r' \1 ', text)
@@ -22,8 +22,7 @@ def prepare_text(text):
     return text
 
 
-def get_fuzzy_pairs(sub_text,ner):
-
+def get_fuzzy_pairs(sub_text, ner):
     if type(sub_text) == list:
         sub_text_split = sub_text
         ner_split = ner
@@ -38,7 +37,7 @@ def get_fuzzy_pairs(sub_text,ner):
         match_st = ''
         for st in sub_text_split:
 
-            score = fuzz.ratio(nt,st)
+            score = fuzz.ratio(nt, st)
             if nt.startswith(st):
                 score += 20
             # print(st,'===>',score)
@@ -46,14 +45,13 @@ def get_fuzzy_pairs(sub_text,ner):
                 match_st = st
                 max_score = score
 
+        r.append((nt, match_st, max_score))
 
-        r.append((nt,match_st,max_score))
-
-    s_index = -1 #= ner_split.index(r)
+    s_index = -1  # = ner_split.index(r)
 
     final_result = []
     for i in range(len(r)):
-        label,text, score = r[i]
+        label, text, score = r[i]
 
         if score >= 60:
             s_index = sub_text_split.index(text)
@@ -64,24 +62,21 @@ def get_fuzzy_pairs(sub_text,ner):
 
     for label, text, score in r:
 
-
-        if score < 60 :
-            sr.append((label,None))
+        if score < 60:
+            sr.append((label, None))
         else:
-            sr.append((label,text))
+            sr.append((label, text))
 
     final_result.append(sr)
-
 
     return final_result
 
 
-def get_range_kent(ner,text):
+def get_range_kent(ner, text):
     # basic tokenizer
     # ner = prepare_text(ner)
     label = ner
     # text_split = prepare_text(text)
-
 
     cc = genCC(text)
     max_score = -1
@@ -89,17 +84,14 @@ def get_range_kent(ner,text):
     for ci in cc:
         new_score = fuzz.ratio(label, ci)
 
-        if new_score > max_score :
+        if new_score > max_score:
             max_score = new_score
             target_str = ci
     # try:
     # print(target_str)
     start = text.index(target_str)
     end = start + len(target_str)
-    return (start,end)
-
-
-
+    return (start, end)
 
 
 def _get_range(ner, text):
@@ -127,6 +119,96 @@ def _get_range(ner, text):
     _, start, end = sorted(history)[-1]
     return (start, end)
 
+def find_sub_list(sl,l):
+    sll=len(sl)
+    for ind in (i for i,e in enumerate(l) if e==sl[0]):
+        if l[ind:ind+sll]==sl:
+            return ind,ind+sll-1
+
+
+# TODO: here !!
+def get_bio_tagging_string(text, street, poi):
+    """
+    LABELS = ['O', 'B-STREET', 'I-STREET', 'B-POI', 'I-POI']
+
+    :param text:
+    :param street:
+    :param poi:
+    :return: BIO Tagging Sting
+    """
+    p_start, p_end, s_start, s_end = get_bio_tagging_range(text, street, poi)
+
+    text_splits = prepare_text(text)
+
+    BIO = ["O"] * len(text_splits)
+
+    if p_start != None:
+
+        p_splits = prepare_text(text[p_start:p_end])
+
+        start,end = find_sub_list(p_splits,text_splits)
+
+        BIO[start] = "B-POI"
+
+        for i in range(start+1,end):
+            BIO[i] = "I-POI"
+
+    if s_start != None:
+        s_splits = prepare_text(text[s_start:s_end])
+
+        start_2,end_2 = find_sub_list(s_splits,text_splits)
+
+        set1 = set(range(start, end))
+
+        set2 = set(range(start_2, end_2))
+        assert len(set1.intersection(set2))==0
+
+        BIO[start] = "B-STREET"
+
+        for i in range(start_2 + 1, end_2):
+            BIO[i] = "I-STREET"
+
+
+
+    return BIO
+
+
+def get_bio_tagging_range(text, street, poi):
+    """
+    取得 stree and poi 在句子中的 range
+
+    :param text:
+    :param street:
+    :param poi:
+    :return:  (poi start, poi end, street start, street end)
+    """
+    if street != None and poi != None:
+        # POI first
+        p_start, p_end = get_range_kent(poi, text)
+
+        p_len = p_end - p_start
+
+        sub_text_1 = text[0:p_start]
+        sub_text_2 = text[p_end:]
+
+        # 判斷在前面還是後面
+        if len(sub_text_1) > len(sub_text_2):
+
+            s_start, s_end = get_range_kent(street, sub_text_1)
+        else:
+            s_start, s_end = get_range_kent(street, sub_text_2)
+            s_start += p_end
+            s_end += p_end
+
+        return (p_start, p_end, s_start, s_end)
+    elif street == None:
+        p_start, p_end = get_range_kent(poi, text)
+        return (p_start, p_end, None, None)
+    elif poi == None:
+        s_start, s_end = get_range_kent(street, text)
+        return (None, None, s_start, s_end)
+
+
 def get_bio_tagging(text, street, poi):
     len_text = len(text)
     bio = ['O'] * len_text
@@ -141,7 +223,7 @@ def get_bio_tagging(text, street, poi):
         if ner is not None:
             len_ner = len(ner)
             for i in range(len_text - len_ner + 1):
-                start, end = get_range_kent(ner,text) #_get_range(ner, text)
+                start, end = get_range_kent(ner, text)  # _get_range(ner, text)
 
                 bio[start] = f'B-{name}'
                 for j in range(start + 1, end):
@@ -160,49 +242,23 @@ def genCC(raw):
     cc = []
     # print(ss)
     for i in range(len(ss)):
-        for j in range(i+1,len(ss)+1):
+        for j in range(i + 1, len(ss) + 1):
             # print(ss[i:j])
 
             temp_str = ss[i]
 
-            for h in range(i+1,j):
+            for h in range(i + 1, j):
                 if ss[h] in """-.,&[]()\"'""":
-                    if (temp_str + ss[h]) in raw : temp_str+= ss[h]
-                    elif (temp_str + " " + ss[h]) in raw : temp_str+= " "+ss[h]
+                    if (temp_str + ss[h]) in raw:
+                        temp_str += ss[h]
+                    elif (temp_str + " " + ss[h]) in raw:
+                        temp_str += " " + ss[h]
                 else:
-                    if temp_str + " " + ss[h] in raw : temp_str = temp_str + " " + ss[h]
-                    else: temp_str = temp_str + ss[h]
+                    if temp_str + " " + ss[h] in raw:
+                        temp_str = temp_str + " " + ss[h]
+                    else:
+                        temp_str = temp_str + ss[h]
             cc.append(temp_str)
 
             # cc.append( " ".join(ss[i:j]))
     return cc
-
-
-
-# label,raw = "hanief sembilan mtr -h", "kuripan hanief semb mtr -h, gajah mada, 58112"
-# print(get_fuzzy_pairs(label,raw))
-#
-#
-# raw = " ".join(['a', 'b', 'c', 'd'])
-# label = " ".join(['ba', '-', 'cc'])
-# print(get_fuzzy_pairs(label,raw))
-
-
-
-
-# # cahaya lestari toko ===> kra raya, no b 88 cahaya lest toko, rw 5 kramat
-# raw = 'kra raya, no b 88 cahaya lest toko, rw 5 kramat'
-# label = 'cahaya lestari toko'
-# print(_get_range(ner=label, text=raw))
-# print(_get_range_kent(ner=label, text=raw))
-#
-#
-# label,raw = "plot ab tour & travel", "plot ab tour & tra brawi, kasihan"
-# print(_get_range(ner=label, text=raw))
-# print(_get_range_kent(ner=label, text=raw))
-#
-#
-#
-# label,raw = "hanief sembilan mtr -h", "kuripan hanief semb mtr -h, gajah mada, 58112"
-# print(_get_range(ner=label, text=raw))
-# print(_get_range_kent(ner=label, text=raw))
--- test_nlp_util.py
+++ test_nlp_util.py
@@ -31,3 +31,44 @@ def test_getPair():
     raw = ['a', 'b', 'c', 'd']
     label = ['ba', 'cc']
     assert nlp_util.get_fuzzy_pairs(raw, label) == [1, [('ba', 'b'), ('cc', 'c')]]
+
+
+def test_get_range_kent():
+
+    label,raw = "hanief sembilan mtr -h", "kuripan hanief semb mtr -h, gajah mada, 58112"
+    result = nlp_util.get_range_kent(label,raw)
+    print(result)
+    assert raw[result[0]:result[1]] == 'hanief semb mtr -h'
+
+def test_get_bio_tagging_range():
+    print("")
+    # case 1:
+    poi,street,raw = "toko bb kids", "raya samb gede", "xxx raya. sa-mb gede, 299 toko bb k&ids yyy",
+    p_start,p_end,s_start,s_end = nlp_util.get_bio_tagging_range(raw,street,poi)
+    print("case1")
+    print("POI==>", raw[p_start:p_end])
+    assert raw[p_start:p_end] == 'toko bb k&ids'
+    print("Street==>", raw[s_start:s_end])
+    assert raw[s_start:s_end] =='raya. sa-mb gede'
+
+    # case 2:
+    poi,street,raw = "toko bb kids", "raya samb gede", " toko bb kids, raya samb gede, 299",
+    p_start,p_end,s_start,s_end = nlp_util.get_bio_tagging_range(raw,street,poi)
+    print("case2")
+    print("POI==>", raw[p_start:p_end])
+    print("Street==>", raw[s_start:s_end])
+
+
+    # case 3:
+    poi,street,raw = "toko bb kids", "raya samb gede", "aaa toko bb kids, raya samb gede, 299",
+    p_start,p_end,s_start,s_end = nlp_util.get_bio_tagging_range(raw,street,poi)
+    print("case3")
+    print("POI==>", raw[p_start:p_end])
+    print("Street==>", raw[s_start:s_end])
+
+def test_get_bio_tagging_string():
+    print("")
+    # case 1:
+    poi, street, raw = "toko bb kids", "raya samb gede", "xxx raya. sa-mb gede, 299 toko bb k&ids yyy",
+    BIO = nlp_util.get_bio_tagging_string(raw, street, poi)
+    print(BIO)
